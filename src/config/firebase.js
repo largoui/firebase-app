631baba3cc3a9a422412117414dbb4149545a2be
largoui <largou.imane.mpsi@gmail.com>
2023-11-06 18:22:02 +0100

    firebase deploy

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0mWtMqsHFn9oVG_nKbNZxDaWxbnHc37M",
+  authDomain: "fir-app-a9cf4.firebaseapp.com",
+  projectId: "fir-app-a9cf4",
+  storageBucket: "fir-app-a9cf4.appspot.com",
+  messagingSenderId: "338349936934",
+  appId: "1:338349936934:web:c0b4afec9929e4c161b1e7"
+};
+
+const app = initializeApp(firebaseConfig);
